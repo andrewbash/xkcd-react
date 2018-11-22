ebda73050706e1003c8541501cf17668bef84964
Andrew Bash <andrew@andrewbash.com>
2018-11-21 21:06:12 -0500

    Simplifying comcData variable

--- src/components/Comic.js
+++ src/components/Comic.js
@@ -2,12 +2,13 @@ import React from 'react';
 
 class Comic extends React.Component {
     render() {
+        const comicData = this.props.comicData;
         return (
             <article className="comic">
-                <h2>{this.props.comicData.safe_title}</h2>
-                <h3>Comic #{this.props.comicData.num}</h3>
-                <img src={this.props.comicData.img} alt={`XKCD #${this.props.comicData.num}`}/>
-                <p>{this.props.comicData.alt}</p>
+                <h2>{comicData.safe_title}</h2>
+                <h3>Comic #{comicData.num}</h3>
+                <img src={comicData.img} alt={`XKCD #${comicData.num}`}/>
+                <p>{comicData.alt}</p>
             </article>
         
         )
